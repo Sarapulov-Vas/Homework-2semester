ef2b2c6545c2ab0cc72183640bc75c11e2d18992
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-02-18 16:57:10 +0300

    Add the task Buble sort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] sortingArray = new int[] {10, 23, 20, 19, 12, 27, 44, 15, 10, 5};
+
+void printArray(int[] array)
+{
+    foreach(var element in array)
+    {
+        Console.Write($"{element} ");
+    }
+}
+
+void bubleSort(int[] sortingArray)
+{
+    for (int i = 0; i<sortingArray.Length; ++i)
+    {
+        for (int j = 1; j<sortingArray.Length; ++j)
+        {
+            if (sortingArray[j-1] >= sortingArray[j])
+            {
+                (sortingArray[j-1], sortingArray[j]) = (sortingArray[j], sortingArray[j-1]);
+            }
+        }
+    }
+}
+
+bubleSort(sortingArray);
+printArray(sortingArray);

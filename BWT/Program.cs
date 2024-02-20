006ac519ee729d345b80ae3cd2c87c95d353b231
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-02-20 17:28:58 +0300

    Added BW transformation function

--- BWT/Program.cs
+++ BWT/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Tuple<string, int> BWT(string inputString){
+    string[] suffixArray = new string[inputString.Length];
+    var resultString= new char[inputString.Length];
+    int endIndex;
+    suffixArray[0] = inputString;
+    for (int i = 1; i<inputString.Length; i++)
+    {   
+        suffixArray[i] = suffixArray[i-1].Remove(0, 1) + suffixArray[i-1][0];
+    }
+    Array.Sort(suffixArray);
+    for (int i = 0; i<inputString.Length; i++)
+    {
+        resultString[i] = suffixArray[i][^1];
+    }
+    endIndex = Array.IndexOf(resultString, inputString[^1]);
+    return Tuple.Create(new string(resultString), endIndex);
+}
+
+
+string? str = Console.ReadLine();
+var resultString = BWT(str + "$");
+Console.WriteLine(resultString.Item1);
+Console.WriteLine(resultString.Item2);

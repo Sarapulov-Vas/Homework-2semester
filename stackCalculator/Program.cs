96b463a31e719254daf4b71851ae425d750d23e8
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-02-25 16:55:29 +0300

    Stack calculator class is implemented

--- stackCalculator/Program.cs
+++ stackCalculator/Program.cs
@@ -1,21 +1,21 @@
 ﻿interface IStack
 {   
-    void Push(int value);
-    int Pop();
+    void Push(double value);
+    double Pop();
     int Length { get; set; }
 }
 class ListStack : IStack
 {
     public int Length { get; set; }
-    public List<int> stack = new List<int>();
-    void IStack.Push(int value)
+    public List<double> stack = new List<double>();
+    void IStack.Push(double value)
     {   
         stack.Add(value);
         Length = stack.Count;
     }
-    int IStack.Pop()
+    double IStack.Pop()
     {
-        int value = stack[^1];
+        double value = stack[^1];
         stack.RemoveAt(stack.Count - 1);
         Length = stack.Count;
         return value;
@@ -25,44 +25,93 @@ class ListStack : IStack
 class ArrayStack : IStack
 {
     public int Length { get; set; }
-    public int[] stack = new int[0];
-    void IStack.Push(int value)
+    public double[] stack = Array.Empty<double>() ;
+    void IStack.Push(double value)
     {   
         Length = stack.Length + 1;
         Array.Resize(ref stack, Length);
         stack[^1] = value;
     }
-    int IStack.Pop()
+    double IStack.Pop()
     {
-        int value = stack[^1];
+        double value = stack[^1];
         Array.Resize(ref stack, Length - 1);
         Length = stack.Length;
         return value;
     }
 }
-
+class StackCalculator
+{
+    public double Calculate(string Expression)
+    {
+        IStack stack = new ListStack();
+        string[] arrayExpression = Expression.Split(' ');
+        double firstNumbre, secondNumber;
+        double epsilon = 0.00001;
+        foreach (var element in arrayExpression)
+        {
+            if (element != "+" && element != "*" && element != "/" && element != "-")
+            {   
+                int number;
+                if (!int.TryParse(element, out number))
+                {
+                    Console.WriteLine("Wrong input!");
+                    return 0;
+                }
+                stack.Push(number);
+            }
+            else if (stack.Length >= 2)
+            {
+                firstNumbre = stack.Pop();
+                secondNumber = stack.Pop();
+                switch (element)
+                {
+                    case "+":
+                        stack.Push(firstNumbre + secondNumber);
+                        break;
+                    case "-":
+                        stack.Push(firstNumbre - secondNumber);
+                        break;
+                    case "*":
+                        stack.Push(firstNumbre * secondNumber);
+                        break;
+                    case "/":
+                        if (Math.Abs(secondNumber) <= epsilon)
+                        {
+                            Console.WriteLine("Division by zero!");
+                            return 0;
+                        }
+                        stack.Push(firstNumbre / secondNumber);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input!");
+                        return 0;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Too many operations");
+                return 0;
+            }
+        }
+        if (stack.Length != 1)
+        {
+            Console.WriteLine("Too much argument!");
+            return 0;
+        }
+        return stack.Pop();
+    }
+}
 class MainClass
 {
     static void Main()
-    {
-        IStack stack = new ListStack();
-        stack.Push(3);
-        stack.Push(2);
-        stack.Push(1);
-        stack.Push(5);
-        Console.WriteLine(stack.Length);
-        while (stack.Length != 0){
-            Console.WriteLine($"{stack.Pop()} {stack.Length}");
-        }
-        Console.WriteLine();
-        IStack ArrStack = new ArrayStack();
-        stack.Push(2);
-        stack.Push(5);
-        stack.Push(6);
-        stack.Push(7);
-        Console.WriteLine(stack.Length);
-        while (stack.Length != 0){
-            Console.WriteLine($"{stack.Pop()} {stack.Length}");
+    {   
+        Console.WriteLine("Input expression ");
+        string? inputString = Console.ReadLine();
+        if (inputString != null)
+        {
+            StackCalculator result = new StackCalculator();
+            Console.WriteLine(result.Calculate(inputString));
         }
     }
 }

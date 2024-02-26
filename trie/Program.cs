2237ed17e29fe2f082fb568b7ea7b4a53288123b
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-02-26 16:55:52 +0300

    The method of element checking is implemented

--- trie/Program.cs
+++ trie/Program.cs
@@ -36,7 +36,26 @@ class Trie
         }
         return false;
     }
-    
+    public bool Contains(string element)
+    {
+        TrieNode currentNode = root;
+        foreach (var symbol in element)
+        {
+            if (!currentNode.NextNode.ContainsKey(symbol))
+            {
+                return false;
+            }
+            currentNode = currentNode.NextNode[symbol];
+        }
+        if (currentNode.IsTerminal)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
 class MainClass
 {
@@ -45,5 +64,10 @@ class MainClass
         Trie trie = new Trie();
         Console.WriteLine(trie.Add("март"));
         Console.WriteLine(trie.Add("маcло"));
+        Console.WriteLine(trie.Add("маcлонасос"));
+        Console.WriteLine(trie.Contains("март"));
+        Console.WriteLine(trie.Contains("маcло"));
+        Console.WriteLine(trie.Contains("мар"));
+        Console.WriteLine(trie.Contains("масса"));
     }
 }

361a7887e125959b2e38e9e346b6804b64ab77ca
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-02-29 20:42:38 +0300

    Correct the error in the comments, minor changes to the variables

--- BWT/Program.cs
+++ BWT/Program.cs
@@ -1,7 +1,7 @@
 ﻿/// <summary>
-/// BWT direct conversion function.
+/// BWT direct conversion function/
 /// </summary>
-/// <param name="inputString">Converted string.<param>
+/// <param name="inputString">Converted string.</param>
 /// <returns>The converted string and the position of the end of the string.</returns>
 (string, int) BWT(string inputString)
 {
@@ -34,37 +34,37 @@
 /// <summary>
 /// Inverse BWT transformation.
 /// </summary>
-/// <param name="BWTstring">Converted string.<param>
-/// <param name="endIndex">End of line position.<param>
+/// <param name="stringBWT">Converted string.</param>
+/// <param name="endIndex">End of line position.</param>
 /// <returns>Original string.</returns>
-string InverseBWT(string BWTString, int endIndex)
+string InverseBWT(string stringBWT, int endIndex)
 {
-    if (BWTString.Length == 0)
+    if (stringBWT.Length == 0)
     {
         return string.Empty;
     }
 
-    char[] characters = BWTString.Distinct().ToArray();
+    char[] characters = stringBWT.Distinct().ToArray();
     int[] characterIndex = new int[characters.Length];
-    int[] sortedIndexes = new int[BWTString.Length];
-    char[] originalString = new char[BWTString.Length];
+    int[] sortedIndexes = new int[stringBWT.Length];
+    char[] originalString = new char[stringBWT.Length];
     Array.Sort(characters);
     for (int i = 1; i < characters.Length; i++)
     {
-        characterIndex[i] = BWTString.Where(sign => sign == characters[i - 1]).Count() + characterIndex[i - 1];
+        characterIndex[i] = stringBWT.Where(sign => sign == characters[i - 1]).Count() + characterIndex[i - 1];
     }
 
-    for (int i = 0; i < BWTString.Length; i++)
+    for (int i = 0; i < stringBWT.Length; i++)
     {
-        sortedIndexes[i] = characterIndex[Array.IndexOf(characters, BWTString[i])];
-        characterIndex[Array.IndexOf(characters, BWTString[i])]++;
+        sortedIndexes[i] = characterIndex[Array.IndexOf(characters, stringBWT[i])];
+        characterIndex[Array.IndexOf(characters, stringBWT[i])]++;
     }
 
-    originalString[^1] = BWTString[endIndex];
-    for (int i = 1; i < BWTString.Length; i++)
+    originalString[^1] = stringBWT[endIndex];
+    for (int i = 1; i < stringBWT.Length; i++)
     {
         endIndex = sortedIndexes[endIndex];
-        originalString[^(i + 1)] = BWTString[endIndex];
+        originalString[^(i + 1)] = stringBWT[endIndex];
     }
 
     return new string(originalString);
@@ -73,10 +73,10 @@ string InverseBWT(string BWTString, int endIndex)
 /// <summary>
 /// Test Function.
 /// </summary>
-/// <param name="inputString">Original string.<param>
-/// <param name="expectedOut">String after conversion.<param>
-/// <param name="expectedPosition">Expected end of line position.<param>
-/// <returns>Is the right result.</returns
+/// <param name="inputString">Original string.</param>
+/// <param name="expectedOut">String after conversion.</param>
+/// <param name="expectedPosition">Expected end of line position.</param>
+/// <returns>Is the right result.</returns>
 bool Test(string inputString, string expectedOut, int expectedPosition)
 {
     var result = BWT(inputString);
@@ -87,12 +87,15 @@ bool Test(string inputString, string expectedOut, int expectedPosition)
 /// <summary>
 /// Running tests.
 /// </summary>
-/// <returns>Test result.</returns
+/// <returns>Test result.</returns>
 bool StartTest()
 {
-    bool[] tests = [Test("KARKARKAR", "KKKRRRAAA", 3),
+    bool[] tests =
+    {
+        Test("KARKARKAR", "KKKRRRAAA", 3),
         Test("ABCabc.ABC/abcABCABC.abc", "cCCC.ccAAAABBBBC/.aaabbb", 6),
-        Test("", "", -1)];
+        Test("", "", -1),
+    };
     bool result = true;
     for (int i = 0; i < 3; i++)
     {

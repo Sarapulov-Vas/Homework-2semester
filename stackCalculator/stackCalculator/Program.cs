3b20cd91e7ec13f46c8c7b5efa37fbdd0f10e6b0
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-03-06 01:15:28 +0300

    create a project for tests

--- stackCalculator/stackCalculator/Program.cs
+++ stackCalculator/stackCalculator/Program.cs
@@ -5,14 +5,6 @@ internal class Program
 {
     private static void Main()
     {
-        Test tests = new Test();
-        Test.AddTest("1 2 3 + *", 5);
-        Test.AddTest("1 2 3 * +", 7);
-        Test.AddTest("2 2 5 / 1 + 7 * / -10 -", -22.25);
-        if (!Test.StartTest())
-        {
-            return;
-        }
 
         Console.WriteLine("Input expression ");
         string? inputString = Console.ReadLine();

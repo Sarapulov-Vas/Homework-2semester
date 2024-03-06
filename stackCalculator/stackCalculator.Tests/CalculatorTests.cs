976bd6d75dcb14c450283c08230a14571ccbe7a5
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-03-06 19:02:59 +0300

    add addition, subtraction, wrong entry, division by zero tests

--- stackCalculator/stackCalculator.Tests/CalculatorTests.cs
+++ stackCalculator/stackCalculator.Tests/CalculatorTests.cs
@@ -14,7 +14,84 @@ public class Tests
     [Test]
     public void TestCorrectInput ()
     {
-        Assert.That(ListStackCalculator.Calculate("1 2 3 * +"), Is.EqualTo(7));
-        Assert.That(ArrayStackCalculator.Calculate("1 2 3 * +"), Is.EqualTo(7));
+        Assert.That(ListStackCalculator.Calculate("1 2 2 4 5 * / + -"), Is.EqualTo(11));
+        Assert.That(ArrayStackCalculator.Calculate("1 2 2 4 5 * / + -"), Is.EqualTo(11));
+    }
+
+    [Test]
+    public void TestDivisionByZero ()
+    {
+        Assert.Throws<DivideByZeroException>(() => ListStackCalculator.Calculate("0 2 /"));
+        Assert.Throws<DivideByZeroException>(() => ArrayStackCalculator.Calculate("0 2 /"));
+    }
+
+    [Test]
+    public void TestIncorrectInput ()
+    {
+        Assert.Throws<ArgumentException>(() => ListStackCalculator.Calculate("0b1 -10e +"));
+        Assert.Throws<ArgumentException>(() => ArrayStackCalculator.Calculate("0b1 -10e +"));
+
+        Assert.Throws<ArgumentException>(() => ListStackCalculator.Calculate("5 10 ="));
+        Assert.Throws<ArgumentException>(() => ArrayStackCalculator.Calculate("5 10 ="));
+
+        Assert.Throws<ArgumentException>(() => ListStackCalculator.Calculate(""));
+        Assert.Throws<ArgumentException>(() => ArrayStackCalculator.Calculate(""));
+    }
+
+    [Test]
+    public void TestMoreArgumentsThanOperations ()
+    {
+        Assert.Throws<Exception>(() => ListStackCalculator.Calculate("1 2 3 +"));
+        Assert.Throws<Exception>(() => ArrayStackCalculator.Calculate("1 2 3 +"));
+    }
+
+    [Test]
+    public void TestMoreOperationsThanArguments ()
+    {
+        Assert.Throws<Exception>(() => ListStackCalculator.Calculate("1 2 + +"));
+        Assert.Throws<Exception>(() => ArrayStackCalculator.Calculate("1 2 + +"));
+    }
+
+    [Test]
+    public void TestAddition ()
+    {
+        Assert.That(ListStackCalculator.Calculate("15 19 +"), Is.EqualTo(34));
+        Assert.That(ArrayStackCalculator.Calculate("15 19 +"), Is.EqualTo(34));
+
+        Assert.That(ListStackCalculator.Calculate("2147483647 2147483647 +"), Is.EqualTo(4294967294));
+        Assert.That(ArrayStackCalculator.Calculate("2147483647 2147483647 +"), Is.EqualTo(4294967294));
+
+        Assert.That(ListStackCalculator.Calculate("-2147483648 -2147483648 +"), Is.EqualTo(-4294967296));
+        Assert.That(ArrayStackCalculator.Calculate("-2147483648 -2147483648 +"), Is.EqualTo(-4294967296));
+
+        Assert.That(ListStackCalculator.Calculate("-2147483648 2 +"), Is.EqualTo(-2147483646));
+        Assert.That(ArrayStackCalculator.Calculate("-2147483648 2 +"), Is.EqualTo(-2147483646));
+
+        Assert.That(ListStackCalculator.Calculate("0 0 +"), Is.EqualTo(0));
+        Assert.That(ArrayStackCalculator.Calculate("0 0 +"), Is.EqualTo(0));
+
+        Assert.That(ListStackCalculator.Calculate("-1 0 +"), Is.EqualTo(-1));
+        Assert.That(ArrayStackCalculator.Calculate("-1 0 +"), Is.EqualTo(-1));
+    }
+    [Test]
+    public void TestDivision ()
+    {
+        Assert.That(ListStackCalculator.Calculate("15 19 -"), Is.EqualTo(4));
+        Assert.That(ArrayStackCalculator.Calculate("15 19 -"), Is.EqualTo(4));
+
+        Assert.That(ListStackCalculator.Calculate("2147483647 2147483647 -"), Is.EqualTo(0));
+        Assert.That(ArrayStackCalculator.Calculate("2147483647 2147483647 -"), Is.EqualTo(0));
+
+        Assert.That(ListStackCalculator.Calculate("-2147483648 -2147483648 -"), Is.EqualTo(0));
+        Assert.That(ArrayStackCalculator.Calculate("-2147483648 -2147483648 -"), Is.EqualTo(0));
+
+        Assert.That(ListStackCalculator.Calculate("-2147483648 2 -"), Is.EqualTo(2147483650));
+        Assert.That(ArrayStackCalculator.Calculate("-2147483648 2 -"), Is.EqualTo(2147483650));
+
+        Assert.That(ListStackCalculator.Calculate("0 0 -"), Is.EqualTo(0));
+        Assert.That(ArrayStackCalculator.Calculate("0 0 -"), Is.EqualTo(0));
+
+        Assert.That(ListStackCalculator.Calculate("-1 0 -"), Is.EqualTo(1));
+        Assert.That(ArrayStackCalculator.Calculate("-1 0 -"), Is.EqualTo(1));
     }
 }

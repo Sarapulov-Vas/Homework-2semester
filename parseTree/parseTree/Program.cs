2bd711f28f3d511eb24b23a88229efbbdd0e57ef
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-03-23 22:56:44 +0300

    Add exception handling.

--- parseTree/parseTree/Program.cs
+++ parseTree/parseTree/Program.cs
@@ -7,8 +7,23 @@ internal static class Program
     {
         Console.Write("Input path of parse tree file: ");
         string path = Console.ReadLine();
-        var parseTree = new ParseTree(path);
-        parseTree.PrintParseTree();
-        Console.WriteLine(parseTree.Calculate());
+        try
+        {
+            var parseTree = new ParseTree(path);
+            parseTree.PrintParseTree();
+            Console.WriteLine(parseTree.Calculate());
+        }
+        catch (IncorrectInputException)
+        {
+            Console.WriteLine("Incorrect input file!");
+        }
+        catch (IncorrectOperandException)
+        {
+            Console.WriteLine("Invalid operand of the parse tree!");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("The expression cannot be calculated because of division by zero!");
+        }
     }
 }

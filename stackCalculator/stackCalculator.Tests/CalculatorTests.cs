9e1d784d0b4e9abf14fbb5be0a6cddb6c217603d
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-04-01 01:53:42 +0300

    change tests

--- stackCalculator/stackCalculator.Tests/CalculatorTests.cs
+++ stackCalculator/stackCalculator.Tests/CalculatorTests.cs
@@ -1,102 +1,95 @@
+using Microsoft.VisualBasic;
+
 namespace stackCalculator.Tests;
 
 public class CalculateorTests
 {
-    readonly double epsilon = 1e-10;
-    private static IEnumerable<TestCaseData> Calculator()
-    {
-        yield return new TestCaseData(new StackCalculator(new ListStack()));
-        yield return new TestCaseData(new StackCalculator(new ArrayStack()));
-    }
+    private readonly double epsilon = 1e-10;
 
-    [TestCaseSource(nameof(Calculator))]
+    
+
+    [TestCaseSource(typeof(TestData), nameof(TestData.Calculator))]
     public void TestCorrectInput (StackCalculator calculator)
     {
         Assert.That(Math.Abs(calculator.Calculate("1 2 2 4 5 * / + -") - 11) < epsilon);
     }
 
-    [TestCaseSource(nameof(Calculator))]
+    [TestCaseSource(typeof(TestData), nameof(TestData.Calculator))]
     public void TestDivisionByZero (StackCalculator calculator)
     {
         Assert.Throws<DivideByZeroException>(() => calculator.Calculate("0 2 /"));
     }
 
-    [TestCaseSource(nameof(Calculator))]
-    public void TestIncorrectInput (StackCalculator calculator)
+    [TestCaseSource(typeof(TestData), nameof(TestData.TestCasesIncorrectInput))]
+    public void TestIncorrectInput (StackCalculator calculator, string inputString)
     {
-        Assert.Throws<ArgumentException>(() => calculator.Calculate("0b1 -10e +"));
-
-        Assert.Throws<ArgumentException>(() => calculator.Calculate("5 10 ="));
-
-        Assert.Throws<ArgumentException>(() => calculator.Calculate(""));
+        Assert.Throws<ArgumentException>(() => calculator.Calculate(inputString));
     }
 
-    [TestCaseSource(nameof(Calculator))]
+    [TestCaseSource(typeof(TestData), nameof(TestData.Calculator))]
     public void TestMoreArgumentsThanOperations (StackCalculator calculator)
     {
         Assert.Throws<Exception>(() => calculator.Calculate("1 2 3 +"));
     }
 
-    [TestCaseSource(nameof(Calculator))]
+    [TestCaseSource(typeof(TestData), nameof(TestData.Calculator))]
     public void TestMoreOperationsThanArguments (StackCalculator calculator)
     {
         Assert.Throws<Exception>(() => calculator.Calculate("1 2 + +"));
     }
 
-    [TestCaseSource(nameof(Calculator))]
-    public void TestAddition (StackCalculator calculator)
+    [TestCaseSource(typeof(TestData), nameof(TestData.TestCasesOperations))]
+    public void TestOperations (StackCalculator calculator, string inputString, double result)
     {
-        Assert.That(Math.Abs(calculator.Calculate("15 19 +") - 34) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("2147483647 2147483647 +") - 4294967294) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("-2147483648 -2147483648 +") - -4294967296) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("-2147483648 2 +") - -2147483646) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("0 0 +")) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("-1 0 +") - -1) < epsilon);
-    }
-    [TestCaseSource(nameof(Calculator))]
-    public void TestSubtraction (StackCalculator calculator)
-    {
-        Assert.That(Math.Abs(calculator.Calculate("15 19 -") - 4) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("2147483647 2147483647 -")) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("-2147483648 -2147483648 -")) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("-2147483648 2 -") - 2147483650) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("0 0 -")) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("-1 0 -") - 1) < epsilon);
+        Assert.That(Math.Abs(calculator.Calculate(inputString) - result) < epsilon);
     }
 
-    [TestCaseSource(nameof(Calculator))]
-    public void TestMultiplication (StackCalculator calculator)
+   
+
+    public class TestData
     {
-        Assert.That(Math.Abs(calculator.Calculate("15 19 *") - 285) < epsilon);
+        private static readonly List<StackCalculator> ListOfCalculators = 
+            new List<StackCalculator>() { new StackCalculator(new ListStack()), new StackCalculator(new ArrayStack()) };
 
-        Assert.That(Math.Abs(calculator.Calculate("111111 -111111 *") - -12345654321) < epsilon);
+        public static IEnumerable<TestCaseData> Calculator()
+        {
+            foreach (var element in ListOfCalculators)
+            {
+                yield return new TestCaseData(element);
+            }
+        }
 
-        Assert.That(Math.Abs(calculator.Calculate("-111111 -111111 *") - 12345654321) < epsilon);
+        public static IEnumerable<TestCaseData> TestCasesIncorrectInput()
+        {
+            string[] DataCase = {"0b1 -10e +", "5 10 =", ""};
+            foreach (var element in ListOfCalculators)
+            {
+                foreach (var input in DataCase)
+                {
+                    yield return new TestCaseData(element, input);
+                }
+            }
+        }
 
-        Assert.That(Math.Abs(calculator.Calculate("0 0 *")) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("-1 0 *")) < epsilon);
-    }
-
-    [TestCaseSource(nameof(Calculator))]
-    public void TestDivision (StackCalculator calculator)
-    {
-        Assert.That(Math.Abs(calculator.Calculate("5 15 /") - 3) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("111111 -111111 /") - -1) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("-111111 -111111 /") - 1) < epsilon);
-
-        Assert.That(Math.Abs(calculator.Calculate("-1 0 *")) < epsilon);
+        public static IEnumerable<TestCaseData> TestCasesOperations()
+        {
+            (string, double)[] DataCase = 
+                [
+                    ("15 19 +", 34), ("2147483647 2147483647 +", 4294967294), ("-2147483648 -2147483648 +", -4294967296),
+                    ("-2147483648 2 +", -2147483646), ("0 0 +", 0), ("-1 0 +", -1),
+                    ("15 19 -", 4), ("2147483647 2147483647 -", 0), ("-2147483648 -2147483648 -", 0),
+                    ("-2147483648 2 -", 2147483650), ("0 0 -", 0), ("-1 0 -", 1),
+                    ("15 19 *", 285), ("111111 -111111 *", -12345654321), ("-111111 -111111 *", 12345654321),
+                    ("0 0 *", 0), ("-1 0 *", 0), ("5 15 /", 3), 
+                    ("111111 -111111 /", -1), ("-111111 -111111 /", 1)
+                ];
+            foreach (var element in ListOfCalculators)
+            {
+                foreach (var input in DataCase)
+                {
+                    yield return new TestCaseData(element, input.Item1, input.Item2);
+                }
+            }
+        }
     }
 }
